b611d121a6c46d4f24dba7f8907a996970e3e419
FadhProj <34374858+FadhProj@users.noreply.github.com>
2025-11-21 20:40:17 +0700

    main

--- Tugas-Besar-IF480x-Kelompok_01/main.cpp
+++ Tugas-Besar-IF480x-Kelompok_01/main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
+#include "main_admin.h"
+#include "main_user.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int option=-99;
+    while (option != 0) {
+        system("cls");
+        cout << "============ Menu ============ " << endl;
+        cout << "|| 1. admin                 ||" << endl;
+        cout << "|| 2. User                  ||" << endl;
+        cout << "|| 0. exit                  ||" << endl;
+        cout << "============================== " << endl;
+        cout << "Choose your option : ";
+        cin >> option;
+        switch(option) {
+           case 1  :
+              cout << "you choose option 1" << endl;
+              menuAdmin();
+              break;
+           case 2  :
+              cout << "you choose option 2" << endl;
+              menuUser();
+              break;
+
+           default :
+              exit(1);
+        }
+    }
+
+
     return 0;
 }
---
+++ Tugas-Besar-IF480x-Kelompok_01/main_admin.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_ADMIN_H_INCLUDED
+#define MAIN_ADMIN_H_INCLUDED
+#include <iostream>
+
+using namespace std;
+
+void menuAdmin();
+void menuParent();
+void menuChild();
+#endif // MAIN_ADMIN_H_INCLUDED
